75c6c7f7ec7afa1e5e009cd932436b889387cfdc
maxym49 <konarski.maks@gmail.com>
2018-05-07 22:10:45 +0200

    *Volume method *Stop method *Progress bar method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,16 @@
 package sample;
 
-import com.jfoenix.controls.JFXSlider;
-import com.jfoenix.controls.JFXSpinner;
-import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.controls.JFXToggleButton;
+import com.jfoenix.controls.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import javax.swing.Timer;
 import java.io.File;
@@ -34,13 +34,17 @@ public class Controller {
     private Timer secondTimer;
     private double tTime = 0;
     private double onePercent;
+    private boolean test = false;
 
 
     @FXML
     private JFXSlider volumeSlider;
 
     @FXML
-    private AnchorPane menu;
+    private AnchorPane parent, menu, main, topBar;
+
+    @FXML
+    private Pane midBar, topMenu;
 
     @FXML
     private JFXTextField whatSongfield;
@@ -49,15 +53,29 @@ public class Controller {
     private ProgressBar progressBar;
 
     @FXML
-    private JFXToggleButton loopToggle;
+    private JFXToggleButton loopToggle, themeToggle;
+
+    @FXML
+    private JFXButton openB;
 
     @FXML
     private JFXSpinner spinner;
+
+    @FXML
+    private Button menuButton, exitButton, previousButton, pauseButton, playButton, nextButton, minimizeB;
+
+
 
 
     @FXML
     void exitProgram(){
             System.exit(0);
+    }
+
+    @FXML
+    void mini(){
+        Stage stage = (Stage) parent.getScene().getWindow();
+        stage.setIconified(true);
     }
 
     @FXML
@@ -70,14 +88,6 @@ public class Controller {
         }
     }
 
-    @FXML
-    void testM(){
-        try {
-            mediaPlayer.setOnPlaying(() -> movingText());
-        }catch (Exception e){
-            System.out.println(e);
-        }
-    }
 
     @FXML
     void movingText() {
@@ -111,14 +121,9 @@ public class Controller {
 
         onePercent = (0.01* mediaPlayer.getTotalDuration().toMillis());
 
-
-
-
-
         timer = new Timer((int)onePercent, e ->{
                 progressBar.setProgress(tTime);
                 tTime = tTime + 0.01;
-           // System.out.println(mediaPlayer.getCurrentTime().toSeconds()/100 );
             if(tTime > 1){
                 try {
                     timer.stop();
@@ -168,12 +173,10 @@ public class Controller {
                     whatSongfield.setText("");
                 if (iterator.hasNext()==true){
                     play();
-                    System.out.println( mediaPlayer.getTotalDuration().toSeconds());
                 }
                 else
                     whatSongfield.setText("No more songs!");
                     mediaPlayer.stop();
-                System.out.println( mediaPlayer.getTotalDuration().toSeconds());
             });
 
         } catch (Exception e) {
@@ -248,6 +251,7 @@ public class Controller {
 
 
     }
+
     @FXML
     void pauseSong(){
         try {
@@ -265,8 +269,6 @@ public class Controller {
 
     }
 
-
-
     @FXML
     void setVolume(){
         try {
@@ -275,8 +277,75 @@ public class Controller {
             System.out.println("The error name:\n"+e);
             whatSongfield.setText("No songs on the list!");
         }
-
     }
+
+    @FXML
+    void setTheme() {
+
+        if (themeToggle.isSelected() != false) {
+
+            menu.setStyle("-fx-background-color: #E87A7A");
+
+            topMenu.setStyle("-fx-background-color: #AE5C5C");
+
+            topBar.setStyle("-fx-background-color: #F18686");
+            topBar.setStyle("-fx-border-color: #CB6B6B");
+
+            main.setStyle("-fx-background-color: #E87A7A");
+
+            midBar.setStyle("-fx-background-color: #E87A7A");
+            midBar.setStyle("-fx-border-color: #CB6B6B");
+
+            openB.setStyle("-fx-background-color: #E87A7A");
+            openB.setStyle("-fx-border-color: #CB6B6B");
+            openB.setTextFill(Color.BISQUE);
+
+            exitButton.setStyle("-fx-background-color: #E87A7A");
+
+            minimizeB.setStyle("-fx-background-color: #E87A7A");
+
+            menuButton.setStyle("-fx-background-color: #E87A7A");
+
+            openB.setStyle("-fx-background-color: #E87A7A");
+
+            previousButton.setStyle("-fx-background-color: #E87A7A");
+            pauseButton.setStyle("-fx-background-color: #E87A7A");
+            playButton.setStyle("-fx-background-color: #E87A7A");
+            nextButton.setStyle("-fx-background-color: #E87A7A");
+        }
+        else if(themeToggle.isDisable() != true) {
+            menu.setStyle("-fx-background-color: #152A37");
+
+            topMenu.setStyle("-fx-background-color: #254A61");
+
+            topBar.setStyle("-fx-background-color: #152A37");
+            topBar.setStyle("-fx-border-color: #283544");
+
+            main.setStyle("-fx-background-color: #152A37");
+
+            midBar.setStyle("-fx-background-color: #152A37");
+            midBar.setStyle("-fx-border-color: #283544");
+
+            openB.setStyle("-fx-background-color: #152A37");
+            openB.setStyle("-fx-border-color: #283544");
+            openB.setStyle("-fx-background-radius: 6");
+            openB.setTextFill(Color.valueOf("#d05a7d"));
+
+            exitButton.setStyle("-fx-background-color: #152A37");
+
+            minimizeB.setStyle("-fx-background-color: #152A37");
+
+            menuButton.setStyle("-fx-background-color: #152A37");
+
+            openB.setStyle("-fx-background-color: #152A37");
+
+            previousButton.setStyle("-fx-background-color: #152A37");
+            pauseButton.setStyle("-fx-background-color: #152A37");
+            playButton.setStyle("-fx-background-color: #152A37");
+            nextButton.setStyle("-fx-background-color: #152A37");
+        }
+    }
+
 
     @FXML
     void setLoop(){
@@ -323,14 +392,13 @@ public class Controller {
     @FXML
     void openDirectory(){
         try {
-
-                fileChooser = new FileChooser();
-                FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("mp3 files (*.mp3)", "*.mp3");
-                fileChooser.getExtensionFilters().add(extensionFilter);
-                selectedFiles = fileChooser.showOpenMultipleDialog(null);
-                iterator = selectedFiles.listIterator();
-                k=0;
-                play();
+            fileChooser = new FileChooser();
+            FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("mp3 files (*.mp3)", "*.mp3");
+            fileChooser.getExtensionFilters().add(extensionFilter);
+            selectedFiles = fileChooser.showOpenMultipleDialog(null);
+            iterator = selectedFiles.listIterator();
+            k=0;
+            play();
 
         }catch (Exception e){
             System.out.println("No file");
